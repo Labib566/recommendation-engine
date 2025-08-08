24fd5ed2af1128eb93c1ad04d6a763c423fa95a0
Nazmul Arefin Labib <77150256+Labib566@users.noreply.github.com>
2025-08-08 15:59:39 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-# Exit on error
-set -o errexit
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run initial model training
-# Note: We don't run populate_db.py here because we already did it manually.
-python train_sklearn_model.py
+git add requirements.txt
+git commit -m "Remove unused libraries to fix build"
+git push
